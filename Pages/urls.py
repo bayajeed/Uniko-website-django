1d8dd5872f359884f4ce9a15966f0135ee2d1b6b
bayajeed <bayajeedhossain@gmail.com>
2025-09-13 19:35:31 +0600

    update with Cnvert image WEBP

--- Pages/urls.py
+++ Pages/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('about/', views.about_view, name='about'),
     path('services/', views.services_view, name='services'),
     path('portfolio/', views.portfolio_view, name='portfolio'),
+    path('ownership/', views.ownership_view, name='ownership'),
+    path('blog/', views.blog_view, name='blog-products'),
+    path('contact/', views.contact_view, name='contact'),
 ]
--- Pages/views.py
+++ Pages/views.py
@@ -11,3 +11,12 @@ def services_view(request):
 
 def portfolio_view(request):
     return render(request, 'pages/portfolio.html')
+
+def ownership_view(request):
+    return render(request, 'pages/ownership.html')
+
+def blog_view(request):
+    return render(request, 'pages/blog/blog-products.html')
+
+def contact_view(request):
+    return render(request, 'pages/contact.html')
